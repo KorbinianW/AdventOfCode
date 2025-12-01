186f7fe985d6daa55e2073db41549d2aecb8c478
Korbinian Waegemann <korbinian.waegemann@scherdel.com>
2025-12-01 12:15:13 +0100

    Day 1 Part 1 complete

--- 2025/Day1/SecretEntrance/Program.cs
+++ 2025/Day1/SecretEntrance/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace SecretEntrance
+﻿using System.Diagnostics;
+using static System.IO.StreamReader;
+
+namespace SecretEntrance
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string[] dialValues = File.ReadAllLines("C:\\Dev\\AdventOfCode\\2025\\Day1\\puzzle_input.txt");
+            //string[] dialValues = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"];
+
+            int currentDialValue = 50;
+            int count = 0;
+
+            foreach (string dialValue in dialValues)
+            {
+                char direction = dialValue[0];
+                int value = int.Parse(dialValue.Remove(0, 1));
+
+                if (direction == 'R')
+                {
+                    currentDialValue = (currentDialValue + value) % 100;
+                }
+
+                if (direction == 'L')
+                {
+                    currentDialValue = (currentDialValue - value) % 100;
+
+                }
+
+                if (currentDialValue == 0)
+                {
+                    count += 1;
+                }
+            }
+            Debug.WriteLine(count);
         }
     }
 }
