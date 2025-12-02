3c34a721094d03969f3989f258f3e43089907583
Korbinian Waegemann <korbinian.waegemann@scherdel.com>
2025-12-02 13:59:59 +0100

    Tag 1 Teil 2

--- 2025/Day1/SecretEntrance/Program.cs
+++ 2025/Day1/SecretEntrance/Program.cs
@@ -6,60 +6,45 @@ namespace SecretEntrance
     {
         static void Main(string[] args)
         {
-            //string[] dialValues = File.ReadAllLines("C:\\Dev\\AdventOfCode\\2025\\Day1\\puzzle_input.txt");
-            string[] dialValues = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"];
+            string[] dialValues = File.ReadAllLines("C:\\Dev\\AdventOfCode\\2025\\Day1\\puzzle_input.txt");
+            //string[] dialValues = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"];
 
-            int currentDialValue = 50;
-            int count = 0;
+            int position = 50;
+            int zeroCount = 0;
 
             foreach (string dialValue in dialValues)
             {
+                int previousPosition = position;
                 char direction = dialValue[0];
-                int value = int.Parse(dialValue.Remove(0, 1));
+                int distance = int.Parse(dialValue.Remove(0, 1));
+
+                int fullRotations = distance / 100;
+                zeroCount += fullRotations;
+                distance %= 100;
 
                 if (direction == 'R')
                 {
-                    if ((currentDialValue + value) >= 100)
-                    {
-                        count += (currentDialValue + value) / 100;
-                    }
-
-                    currentDialValue = (currentDialValue + value) % 100;
+                    position += distance;
                 }
-
-                if (direction == 'L')
+                else
                 {
-                    if ((currentDialValue - value) <= 0)
+                    position -= distance;
+                    if (position < 0)
                     {
-                        if (currentDialValue != 0)
-                        {
-                            count += 1;
-                        }
-
-                        if (currentDialValue == 0 && ((currentDialValue - value) % 100) == 0)
-                        {
-                            count += -(currentDialValue - value) / 100;
-                            currentDialValue = (-(currentDialValue - value) % 100);
-                        }
-                        else
-                        {
-                            count += -(currentDialValue - value) / 100;
-                            currentDialValue = 100 + ((currentDialValue - value) % 100);
-                        }
+                        position += 100;
                     }
-                    else
-                    {
-                        currentDialValue = ((currentDialValue - value) % 100);
-                    }
-
                 }
 
-                //if (currentDialValue == 0)
-                //{
-                //    count += 1;
-                //}
+                position %= 100;
+
+                if (position == 0 || previousPosition != 0 && (
+                (previousPosition > position && direction == 'R') ||
+                (previousPosition < position && direction == 'L')))
+                {
+                    zeroCount++;
+                }
             }
-            Debug.WriteLine(count);
+            Debug.WriteLine(zeroCount);
         }
     }
 }
